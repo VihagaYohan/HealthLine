6f7d2f50d4f2b533a66f765a953308fbac882542
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-10 15:21:59 +0530

    Added - Shapes and custom fonts and font types

--- app/src/main/java/com/techtribeservices/helathline/ui/theme/Theme.kt
+++ app/src/main/java/com/techtribeservices/helathline/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -260,6 +261,7 @@ fun HelathLineTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        shapes = Shapes
     )
 }
