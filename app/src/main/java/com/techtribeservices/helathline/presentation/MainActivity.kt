0f4682d660ffd65c6d426ac5b2e664c9dbb10e75
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-10 08:38:02 +0530

    Hilt initiated

--- app/src/main/java/com/techtribeservices/helathline/presentation/MainActivity.kt
+++ app/src/main/java/com/techtribeservices/helathline/presentation/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.techtribeservices.helathline.ui.theme.HelathLineTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
