4aa8ffd0683125314b6dd4f52ce5fbefca28c0c9
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-14 19:44:11 +0530

    Added - Implementation of onboarding screen

--- app/src/main/java/com/techtribeservices/helathline/utils/Constants.kt
+++ app/src/main/java/com/techtribeservices/helathline/utils/Constants.kt
@@ -1,5 +1,12 @@
 package com.techtribeservices.helathline.utils
 
+import androidx.compose.ui.unit.dp
+
 object Constants {
     val TAG: String = "HealthLine"
+
+    val PADDING_SMALL = 5.dp
+    val PADDING_MEDIUM = 10.dp
+    val PADDING_LARGE = 20.dp
+    val PADDING_EXTRA_LARGE = 30.dp
 }
