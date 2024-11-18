2ef3c0a81dfd7d8ee893aa481d4137762caacfdc
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-18 15:59:48 +0530

    Added - ScreenSize util

--- app/src/main/java/com/techtribeservices/helathline/presentation/pages/Onboarding/OnboardingItems.kt
+++ app/src/main/java/com/techtribeservices/helathline/presentation/pages/Onboarding/OnboardingItems.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -23,11 +27,13 @@ import androidx.compose.ui.unit.dp
 import com.techtribeservices.helathline.data.model.Onboarding
 import com.techtribeservices.helathline.ui.theme.poppinsFamily
 import com.techtribeservices.helathline.utils.Constants
+import com.techtribeservices.helathline.utils.ScreenSize
 
 @Composable
 fun OnboardingItem(
     onboardingItem: Onboarding,
 ) {
+    val deviceScreen = ScreenSize()
     Column(
         modifier = Modifier
             .padding(Constants.PADDING_LARGE)
@@ -36,12 +42,19 @@ fun OnboardingItem(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Image(
-            painter = painterResource(onboardingItem.image),
-            contentDescription = "${onboardingItem.title}",
+        Box(
             modifier = Modifier
-
-        )
+                .fillMaxWidth()
+                .height(deviceScreen.height/2)
+        ) {
+            Image(
+                painter = painterResource(onboardingItem.image),
+                contentDescription = "${onboardingItem.title}",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
 
         Spacer(
             modifier = Modifier
