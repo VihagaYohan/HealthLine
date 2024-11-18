ea4bf3ea847d5f8a5e7387a4b509bb8396c5c9ce
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-18 18:32:32 +0530

    Added - top user headline in home screen

--- app/src/main/java/com/techtribeservices/helathline/presentation/pages/Home/HomePage.kt
+++ app/src/main/java/com/techtribeservices/helathline/presentation/pages/Home/HomePage.kt
@@ -1,6 +1,9 @@
 package com.techtribeservices.helathline.presentation.pages.Home
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,10 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.techtribeservices.helathline.data.model.Speciality
+import com.techtribeservices.helathline.presentation.components.UserHeader
 import com.techtribeservices.helathline.presentation.viewModels.HomeViewModel
 
 @Composable
@@ -26,17 +32,19 @@ fun HomePage(
     val uiState = viewModel.state.collectAsState()
 
     Scaffold(
-        modifier = modifier
+        modifier = modifier,
+        topBar = {
+            UserHeader()
+        }
     ) { innerPadding ->
         val modifier = Modifier.padding(innerPadding)
 
         Column(
             modifier = modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxSize()
         ) {
             Text(text = "Home page")
+
         }
 
     }
