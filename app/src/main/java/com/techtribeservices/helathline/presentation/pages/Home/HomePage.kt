b39d9b3fecf3778ce4b6f32105f1477018329cf9
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-19 10:12:43 +0530

    Added - Banner added to home page

--- app/src/main/java/com/techtribeservices/helathline/presentation/pages/Home/HomePage.kt
+++ app/src/main/java/com/techtribeservices/helathline/presentation/pages/Home/HomePage.kt
@@ -1,36 +1,15 @@
 package com.techtribeservices.helathline.presentation.pages.Home
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Menu
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import com.techtribeservices.helathline.data.model.Speciality
-import com.techtribeservices.helathline.presentation.components.Carousel
-import com.techtribeservices.helathline.presentation.components.TextInputField
+import com.techtribeservices.helathline.presentation.components.Banner
 import com.techtribeservices.helathline.presentation.components.UserHeader
 import com.techtribeservices.helathline.presentation.viewModels.HomeViewModel
 import com.techtribeservices.helathline.utils.Constants
@@ -57,8 +36,8 @@ fun HomePage(
                 .padding(Constants.PADDING_MEDIUM)
         ) {
 
-            // carousel
-            Carousel()
+            // banner
+            Banner()
 
         }
 
