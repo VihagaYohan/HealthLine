90519e15c0eaaa6d76e96b030af28400ba1aeec3
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-11-26 20:37:15 +0530

    Added - singleton coil module

--- app/src/main/java/com/techtribeservices/helathline/HealthLineApp.kt
+++ app/src/main/java/com/techtribeservices/helathline/HealthLineApp.kt
@@ -1,8 +1,31 @@
 package com.techtribeservices.helathline
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.request.CachePolicy
+import coil3.request.crossfade
+import coil3.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HealthLineApp: Application() {
+class HealthLineApp: Application(), SingletonImageLoader.Factory {
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader.Builder(context)
+            .crossfade(true)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.03)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
+    }
 }
