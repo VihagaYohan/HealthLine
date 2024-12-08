36dbdd2df4b1389feac9833e19dadd405b65ab09
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-12-08 12:22:09 +0530

    Added - UI changes to schedule tab in doctor details page

--- app/src/main/java/com/techtribeservices/helathline/utils/Constants.kt
+++ app/src/main/java/com/techtribeservices/helathline/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.techtribeservices.helathline.utils
 
 import androidx.compose.ui.unit.dp
+import com.techtribeservices.helathline.data.model.ScheduleDay
 
 object Constants {
     val TAG: String = "HealthLine"
@@ -11,4 +12,8 @@ object Constants {
     val PADDING_EXTRA_LARGE = 30.dp
 
     val DOCTOR_DETAILS_TABS = listOf("Schedule","About", "Review")
+
+    val SCHEDULE_DATES = listOf(ScheduleDay("Mon", 6), ScheduleDay("Tue", 7), ScheduleDay("Wed", 8), ScheduleDay("Thu", 9), ScheduleDay("Fri", 10), ScheduleDay("Sat", 11), ScheduleDay("Sun", 12))
+
+    val SCHEDULE_TIMES = listOf("9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM")
 }
